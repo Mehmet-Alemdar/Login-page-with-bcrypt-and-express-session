9df1324754c17a319e5212db5c8d2ecdaa83232b
Mehmet Alemdar <60268874+Mehmet-Alemdar@users.noreply.github.com>
2022-04-08 19:23:02 +0300

    Create routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { registerRouter, loginRouter, profileRouter } = require("./routes")
 
 require("./mongo-connection")
 
@@ -6,5 +7,13 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use("/register", registerRouter)
+app.use("/login", loginRouter)
+app.use("/profile", profileRouter)
+
+app.get("/", (req,res) => {
+  res.redirect("/login")
+})
 
 module.exports = app
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const registerRouter = require("./register")
+const loginRouter = require("./login")
+const profileRouter = require("./profile")
+
+module.exports = {
+  registerRouter,
+  loginRouter,
+  profileRouter
+}
