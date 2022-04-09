5645f5f2cc81b44cee804bc9d7f22386d7a5be78
Mehmet Alemdar <60268874+Mehmet-Alemdar@users.noreply.github.com>
2022-04-09 07:50:58 +0300

    Generate hashed password

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,7 +1,22 @@
 const router = require("express").Router()
+const createHashedPassword = require("../lib/bcrypt")
 
 router.get("/", (req,res) => {
   res.send("register page")
+})
+
+router.post("/", async (req,res) => {
+  try{
+    const object = req.body
+  
+    const hashedPassword = await createHashedPassword(object.password)
+  
+    object.password = hashedPassword
+  
+    res.status(201).json({msg: "User created"})
+  }catch(err) {
+    res.send({msg: err})
+  }
 })
 
 module.exports = router
