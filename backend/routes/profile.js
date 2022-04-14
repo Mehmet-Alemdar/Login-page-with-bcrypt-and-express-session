ce4728107cb7867ddc6ad65e7b0d117e27127738
Mehmet Alemdar <60268874+Mehmet-Alemdar@users.noreply.github.com>
2022-04-14 23:33:18 +0300

    Add profile status codes

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -4,16 +4,16 @@ const { UserService } = require("../services")
 router.get("/", async(req,res) => {
   try {
     const { id } = req.query
-
+    
     const user = await UserService.find(id)
     
     if(user) {
-      res.send(user)
+      res.status(200).send(user)
     }else{
-      res.status(203).json({msg: "The user not found at this id"})
+      res.status(203).send("The user not found at this id")
     }
   }catch (err) {
-    res.status(203).json({msg: "The user not found at this id"})
+    res.status(203).send("The user not found at this id")
   }
 })
 
